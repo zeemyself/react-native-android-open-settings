65d2a746bd9e7135b153714a46e6d3ebc8480754
Swas zeemyself Kunakorn <swas@wongnai.com>
2018-09-05 12:21:07 +0700

    Fix bug

--- index.js
+++ index.js
@@ -36,7 +36,7 @@ const accessibilitySettings = () => RNAndroidOpenSettings.accessibilitySettings(
 
 const applicationSettings = () => RNAndroidOpenSettings.applicationSettings()
 
-const applicationSelfSettings = package => RNAndroidOpenSettings.applicationSelfSettings(package)
+const applicationSelfSettings = packageName => RNAndroidOpenSettings.applicationSelfSettings(packageName)
 
 const deviceInfoSettings = () => RNAndroidOpenSettings.deviceInfoSettings()
 
